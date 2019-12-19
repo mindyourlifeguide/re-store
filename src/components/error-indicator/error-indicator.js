5d9acff5b7d5afdede8772c36ada1d88973956b3
bohdan <mind.your.life.guide@gmail.com>
2019-12-19 22:34:40 +0200

    0.01 Start project

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './error-indicator.css';
+
+const ErrorIndicator = () => {
+	return (
+		<div className="error-indicator">
+			<h3> Ooops...Something is wrong. </h3>
+		</div>
+	);
+};
+
+export default ErrorIndicator;
